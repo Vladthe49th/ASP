8ddc728aff124b9d1f9c59443dda96a5c476cac2
User <kenobi88@gmail.com>
2026-02-02 13:24:24 +0200

    1-commit

---
+++ MVC-films/MVC-films/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.Data;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+    }
+}
---
+++ MVC-films/MVC-films/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+using System.Collections.Generic;
+
+namespace MovieApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
