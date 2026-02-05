7dbc2f0b57341468351f985bbd4b749b5a19c1d5
User <kenobi88@gmail.com>
2026-02-05 18:43:01 +0200

    1-commit

---
+++ MVC-Filmss/MVC-Filmss/Data/DbInitializer.cs
@@ -0,0 +1,111 @@
+﻿using MovieApp.Models;
+
+namespace MovieApp.Data
+{
+    public static class DbInitializer
+    {
+        public static void Seed(AppDbContext context)
+        {
+            if (context.Movies.Any())
+                return;
+
+            var movies = new List<Movie>
+            {
+                new Movie
+                {
+                    Title = "Володар Перснів: Братство Кільця",
+                    Director = "Пітер Джексон",
+                    Genre = "Фентезі",
+                    Year = 2001,
+                    PosterPath = "/images/movies/lotr.jpg",
+                    Description = "Початок великої історії про подорож, дружбу, та боротьбу зі злом."
+                },
+                new Movie
+                {
+                    Title = "Гаррі Поттер і Таємна Кімната",
+                    Director = "Кріс Коламбус",
+                    Genre = "Фентезі",
+                    Year = 2002,
+                    PosterPath = "/images/movies/hp2.jpg",
+                    Description = "Другий рік Гаррі у Гоґвортсі стає смертельно небезпечним, частково завдяки цьому маньяку Доббі..."
+                },
+                new Movie
+                {
+                    Title = "Сяйво",
+                    Director = "Стенлі Кубрик",
+                    Genre = "Жахи",
+                    Year = 1980,
+                    PosterPath = "/images/movies/shining.jpg",
+                    Description = "Мила сімейна кумедія про батька, який дуже любить дружину і сина."
+                },
+                new Movie
+                {
+                    Title = "Темний Лицар",
+                    Director = "Крістофер Нолан",
+                    Genre = "Екшн",
+                    Year = 2008,
+                    PosterPath = "/images/movies/dark_knight.jpg",
+                    Description = "Бетмен проти хаосу, ім'я якому Джокер."
+                },
+                new Movie
+                {
+                    Title = "My Little Pony: The Movie",
+                    Director = "Джейсон Тіссен",
+                    Genre = "Анімація",
+                    Year = 2017,
+                    PosterPath = "/images/movies/mlp.jpg",
+                    Description = "Тому що кожному топу потрібно таке диво!"
+                },
+                new Movie
+                {
+                    Title = "Термінатор 2: Судний день",
+                    Director = "Джеймс Кемерон",
+                    Genre = "Фантастика",
+                    Year = 1991,
+                    PosterPath = "/images/movies/t2.jpg",
+                    Description = "І ВОССТАНУТЬ МАШИНИ З ПЕПЛУ ЯДЕРНОГО ВОГНЮ",
+                     TrailerUrl = "https://www.youtube.com/embed/CRRlbK5w8AE"
+                },
+                new Movie
+                {
+                    Title = "Кунг-Фу Панда",
+                    Director = "Марк Осборн",
+                    Genre = "Анімація",
+                    Year = 2008,
+                    PosterPath = "/images/movies/kungfu_panda.jpg",
+                    Description = "Ця битва буде легендарною!"
+                },
+                new Movie
+                {
+                    Title = "Походження Імператора",
+                    Director = "Марк Діндал",
+                    Genre = "Анімація",
+                    Year = 2000,
+                    PosterPath = "/images/movies/emperors_new_groove.jpg",
+                    Description = "Не той ричаг, кронк!"
+                },
+                new Movie
+                {
+                    Title = "Розкрадачка гробниць",
+                    Director = "Саймон Вест",
+                    Genre = "Пригоди",
+                    Year = 2001,
+                    PosterPath = "/images/movies/tombraider.jpg",
+                    Description = "Лара Крофт у своєму піку - во всіх сенсах"
+                },
+                new Movie
+                {
+                    Title = "Джуманджі",
+                    Director = "Джо Джонстон",
+                    Genre = "Пригоди",
+                    Year = 1995,
+                    PosterPath = "/images/movies/jumanji.jpg",
+                    Description = "Гра, яку я б не починав."
+                }
+            };
+
+            context.Movies.AddRange(movies);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MVC-Filmss/MVC-Filmss/Migrations/20260205163207_AddTrailerUrlToMovie.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Filmss.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrailerUrlToMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrailerUrl",
+                table: "Movies",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrailerUrl",
+                table: "Movies");
+        }
+    }
+}
