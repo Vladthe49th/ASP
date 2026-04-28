088c116dc83f15c669e0e272694e9444efac7e89
User <kenobi88@gmail.com>
2026-04-28 15:52:44 +0300

    1-commit

---
+++ DjangoCompany/DjangoCompany/Main/Views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def news(request):
+    return render(request, 'main/news.html')
+
+def management(request):
+    return render(request, 'main/management.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
